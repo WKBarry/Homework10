0a9f2e908b059ede09f3a4e9f0cb66d440b853e3
WKBarry <winamkamalgo@gmail.com>
2025-09-26 21:51:42 +0000

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,12 @@
+decimalnumber = int(input("Enter a decimal number: "))
+def decimal_to_binary_manual(n):
+    binary = ""
+    if n == 0:
+        return "0"
+    while n > 0:
+        remainder = n % 2
+        binary = str(remainder) + binary
+        n = n // 2
+    return binar
+binarynumber = decimal_to_binary_manual(decimalnumber)
+print("Binary number is: ",binarynumber)
